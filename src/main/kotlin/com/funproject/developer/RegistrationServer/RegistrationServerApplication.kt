8759f84a0a2fe5b6173d8beb98384442429067bc
nestok <sych_y@mail.ru>
2018-08-30 11:07:35 +0300

    Added config server and configs for clients

--- src/main/kotlin/com/funproject/developer/RegistrationServer/RegistrationServerApplication.kt
+++ src/main/kotlin/com/funproject/developer/RegistrationServer/RegistrationServerApplication.kt
@@ -1,12 +1,26 @@
 package com.funproject.developer.RegistrationServer
 
+import com.netflix.appinfo.InstanceInfo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceRegisteredEvent
+import org.springframework.context.ApplicationListener
 
 @SpringBootApplication
 @EnableEurekaServer
-class RegistrationServerApplication
+@EnableConfigServer
+class RegistrationServerApplication : ApplicationListener<EurekaInstanceRegisteredEvent> {
+
+    override fun onApplicationEvent(instance: EurekaInstanceRegisteredEvent) {
+//        val instanceInfo = instance.instanceInfo
+//        val builder = InstanceInfo.Builder(instanceInfo)
+//        builder.setPort(8090)
+//        instance.instanceInfo = builder.build()
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<RegistrationServerApplication>(*args)
